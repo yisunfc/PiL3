a68f262c14142f65051f35e61475ade49baf3fcd
David <david@MacBook-Pro.local>
2015-11-12 12:47:32 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ProgrammingInLua
+
+Programming In Lua (3rd Edition)的读书笔记
+每个文件里纪录一些知识点或者代码
+文字以注释方式呈现，为的是可以直接执行lua文件
---
+++ chap1.lua
@@ -0,0 +1,5 @@
+--[[You can use the -i option to instruct Lua to start an interactive session after running the given chunk:
+% lua -i prog
+--]]
+
+print(arg[0])
